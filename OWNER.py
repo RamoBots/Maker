f698a605e329b57a353370af06dcad125d710193
Ramo <albearatef@gmail.com>
2024-06-18 10:35:48 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,7 +2,7 @@
 OWNER = ["C_lxl_C", "C_lxl_C"]
 OWNER_NAME = "𑲯𝑹𝑨𝑴𝑶 || رامـــــــو"
 BOT_TOKEN = "7174991352:AAG-iNnmiUyN6U4b1cPQN4IdHx2R9QYLmk8"
-DATABASE = "mongodb+srv://vv1vbvg:tahLdAddP2xj6R5b@cluster0.4xpikbs.mongodb.net/?retryWrites=true&w=majority"
+DATABASE = "mongodb+srv://proceed58:proceed58@cluster0.p5s9ym5.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/R_surce"
 GROUP = "https://t.me/UY_FU"
 VIDEO = "https://t.me/VIP_ALBEAR/24"
